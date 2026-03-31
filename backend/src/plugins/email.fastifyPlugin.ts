c4d0130e992f803de2bc6609ecc44f893e348984
BubuGagaMan <bojidar_tenev@yahoo.com>
2026-03-31 20:02:22 +0100

    add a check for test environmend to isable resend emails

--- backend/src/plugins/email.fastifyPlugin.ts
+++ backend/src/plugins/email.fastifyPlugin.ts
@@ -10,25 +10,33 @@ export type ResendEmailParams = {
 }
 
 const emailPlugin: FastifyPluginAsync = async (fastify: FastifyInstance) => {
-    const resend = new Resend(process.env.RESEND_API_KEY || 'noKey')
+    const isTestEnv = process.env.NODE_ENV === 'test'
+    const resend = isTestEnv ? null : new Resend(process.env.RESEND_API_KEY || 'noKey')
 
     fastify.decorate('sendEmail', async ({ from, to, subject, html }: ResendEmailParams) => {
-        try {
-            const { data, error } = await resend.emails.send({
-                from,
-                to,
-                subject,
-                html,
-            })
+        if (resend) {
+            try {
+                const { data, error } = await resend.emails.send({
+                    from,
+                    to,
+                    subject,
+                    html,
+                })
 
-            if (error) {
-                throw new Error(error.message)
+                if (error) {
+                    throw new Error(error.message)
+                }
+                return data
+            } catch (err) {
+                const errMessage = err instanceof Error ? err.message : 'Unkown error when attempting to send email'
+                fastify.log.error(errMessage)
+                throw new Error(errMessage)
             }
-            return data
-        } catch (err) {
-            const errMessage = err instanceof Error ? err.message : 'Unkown error when attempting to send email'
-            fastify.log.error(errMessage)
-            throw new Error(errMessage)
+        }
+
+        if (isTestEnv) {
+            fastify.log.info(`[MOCK EMAIL] Simulated sending email to: ${to} | Subject: ${subject}`)
+            return { id: 'mock-email-id' } // Return a dummy success object
         }
     })
 }
