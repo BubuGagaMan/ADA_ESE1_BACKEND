d553c754a3d2d9867da260b80835566ba457d8a8
BubuGagaMan <bojidar_tenev@yahoo.com>
2026-03-31 17:23:09 +0100

    add food routes

---
+++ backend/src/routes/food/food.routes.ts
@@ -0,0 +1,20 @@
+import FoodController from '@controllers/food/food.controller.js'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+
+export default async function foodR(app: FastifyInstance) {
+    const foodController = new FoodController()
+
+    app.post(
+        '/food',
+        {
+            onRequest: [
+                app.authenticate,
+                async (req: FastifyRequest, reply: FastifyReply) => {
+                    await app.authorise(req, reply, ['ADMIN'])
+                },
+            ],
+        },
+        foodController.create,
+    )
+    app.get('/food', foodController.getAllFoods)
+}
