7d6270ed476448812737e004d8833ab3952d940f
BubuGagaMan <bojidar_tenev@yahoo.com>
2026-03-31 18:06:10 +0100

    fix food post request types

--- backend/src/routes/food/food.routes.ts
+++ backend/src/routes/food/food.routes.ts
@@ -1,10 +1,11 @@
 import FoodController from '@controllers/food/food.controller.js'
+import { CreateFoodRequest } from '@controllers/food/types.js'
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 
 export default async function foodR(app: FastifyInstance) {
     const foodController = new FoodController()
 
-    app.post(
+    app.post<CreateFoodRequest>(
         '/food',
         {
             onRequest: [
